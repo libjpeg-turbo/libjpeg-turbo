a8c282e5e5ac10a715d6d6a9ab22121982b485f6
mayeut <mayeut@users.noreply.github.com>
2016-02-05 01:59:58 +0100

    Merged huffman ARM64 SIMD implementation Use ZigZag tbl/tblx on iOS Android
    ARM64 Huffman SIMD frequency condition
    
    This adds a conditional path depending on high/low-frequency detection.
    
    Comparison against libjpeg-turbo@fe11699d90c8dd4feac5bb3e1d87cc310b22712a
    
    Performances on ThunderX are still not as good as C implementation:
    vgl_6434_0018a.bmp 100 444 -7%
    vgl_6434_0018a.bmp 95 444 -5%
    vgl_6434_0018a.bmp 95 422 -5%
    vgl_6434_0018a.bmp 95 420 -5%
    artificial.ppm 100 444 -5%
    artificial.ppm 95 444 -9%
    artificial.ppm 95 422 -7%
    artificial.ppm 95 420 -8%
    nightshot_iso_100.ppm 100 444 -4%
    nightshot_iso_100.ppm 95 444 -14%
    nightshot_iso_100.ppm 95 422 -13%
    nightshot_iso_100.ppm 95 420 -12%
    vgl_6434_0018a.bmp 10 444 -12%
    vgl_6434_0018a.bmp 50 444 -11%
    artificial.ppm 10 444 -8%
    artificial.ppm 50 444 -9%
    nightshot_iso_100.ppm 10 444 -7%
    nightshot_iso_100.ppm 50 444 -9%
    
    Performances on iPhone5S are much better for low-frequencies:
    artificial.ppm 100 444 +39%
    artificial.ppm 95 444 +36%
    artificial.ppm 95 422 +39%
    artificial.ppm 95 420 +39%
    artificial.ppm 10 444 +31%
    artificial.ppm 50 444 +31%
    nightshot_iso_100.ppm 100 444 +68%
    nightshot_iso_100.ppm 95 444 +35%
    nightshot_iso_100.ppm 95 422 +39%
    nightshot_iso_100.ppm 95 420 +45%
    nightshot_iso_100.ppm 10 444 +36%
    nightshot_iso_100.ppm 50 444 +37%

--- simd/jsimd_arm64.c
+++ simd/jsimd_arm64.c
@@ -657,6 +657,16 @@ jsimd_idct_float (j_decompress_ptr cinfo, jpeg_component_info * compptr,
 GLOBAL(int)
 jsimd_can_huff_encode_one_block (void)
 {
+  init_simd();
+
+  if (DCTSIZE != 8)
+    return 0;
+  if (sizeof(JCOEF) != 2)
+    return 0;
+
+  if (simd_support & JSIMD_ARM_NEON)
+    return 1;
+
   return 0;
 }
 
@@ -665,5 +675,6 @@ jsimd_huff_encode_one_block (void * state, JOCTET *buffer, JCOEFPTR block,
                              int last_dc_val, c_derived_tbl *dctbl,
                              c_derived_tbl *actbl)
 {
-  return NULL;
+  return jsimd_huff_encode_one_block_neon(state, buffer, block, last_dc_val,
+                                          dctbl, actbl);
 }
