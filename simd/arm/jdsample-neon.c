4ebe08fae480254f19103c8d870251508b4c526a
Jonathan Wright <jonathan.wright@arm.com>
2018-06-28 16:17:36 +0100

    Add Arm NEON implementation of h2v1_upsample
    Adds an Arm NEON intrinsics implementation of h2v1_upsample. This is
    new code, there was no previous NEON assembly implementation for
    either AArch32 or AArch64.
    
    Change-Id: I1297ca6c66e19ac1211302d88f3f579fa0971a69

--- simd/arm/jdsample-neon.c
+++ simd/arm/jdsample-neon.c
@@ -472,3 +472,39 @@ void jsimd_h1v2_fancy_upsample_neon(int max_v_samp_factor,
     }
   }
 }
+
+
+/*
+ * The diagram below shows the operation of h2v1 (simple) upsampling. Each
+ * sample in the row is duplicated to form two output pixel channel values.
+ *
+ *                          p0   p1   p2   p3
+ *     +----+----+        +----+----+----+----+
+ *     | s0 | s1 |   ->   | s0 | s0 | s1 | s1 |
+ *     +----+----+        +----+----+----+----+
+ */
+
+void jsimd_h2v1_upsample_neon(int max_v_samp_factor,
+                              JDIMENSION output_width,
+                              JSAMPARRAY input_data,
+                              JSAMPARRAY *output_data_ptr)
+{
+  JSAMPARRAY output_data = *output_data_ptr;
+  JSAMPROW inptr, outptr;
+
+  for (int inrow = 0; inrow < max_v_samp_factor; inrow++) {
+    inptr = input_data[inrow];
+    outptr = output_data[inrow];
+    for (unsigned colctr = 0; 2 * colctr < output_width; colctr += 16) {
+      uint8x16_t samples = vld1q_u8(inptr + colctr);
+      /* Duplicate the samples - the store interleaves them to produce the */
+      /* pattern in the diagram above. */
+      uint8x16x2_t output_pixels = { samples, samples };
+      /* Store pixel values to memory. */
+      /* Due to the way sample buffers are allocated, we don't need to worry */
+      /* about tail cases when output_width is not a multiple of 32. */
+      /* See "Creation of 2-D sample arrays" in jmemmgr.c for details. */
+      vst2q_u8(outptr + 2 * colctr, output_pixels);
+    }
+  }
+}
