13f9c25a0e7e4f09d77c493c81af9fc61091f48e
Jonathan Wright <jonathan.wright@arm.com>
2018-08-22 13:38:37 +0100

    Implement accurate DCT using Arm Neon intrinsics
    Adds and Arm Neon intrinsics implementation of the slower but more
    accurate Discrete Cosine Transform (DCT).
    
    Removes the Neon assembly implementation for AArch64. There was no
    Neon implementation for AArch32.
    
    Change-Id: Ic35dfa066ae3092d5ad9164e2de0d01a725f106e

--- simd/arm/aarch32/jsimd.c
+++ simd/arm/aarch32/jsimd.c
@@ -564,6 +564,17 @@ jsimd_convsamp_float(JSAMPARRAY sample_data, JDIMENSION start_col,
 GLOBAL(int)
 jsimd_can_fdct_islow(void)
 {
+  init_simd();
+
+  /* The code is optimised for these values only */
+  if (DCTSIZE != 8)
+    return 0;
+  if (sizeof(DCTELEM) != 2)
+    return 0;
+
+  if (simd_support & JSIMD_NEON)
+    return 1;
+
   return 0;
 }
 
@@ -593,6 +604,7 @@ jsimd_can_fdct_float(void)
 GLOBAL(void)
 jsimd_fdct_islow(DCTELEM *data)
 {
+  jsimd_fdct_islow_neon(data);
 }
 
 GLOBAL(void)
