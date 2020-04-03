dd33c9bff599ab436e884801ba57fe46fb7d97cd
Ruan Kunliang <peterrk@126.com>
2020-04-03 21:49:58 +0800

    reuse output buffer in jpeg_stdio_dest

--- jdatadst.c
+++ jdatadst.c
@@ -71,8 +71,9 @@ init_destination(j_compress_ptr cinfo)
   my_dest_ptr dest = (my_dest_ptr)cinfo->dest;
 
   /* Allocate the output buffer --- it will be released when done with image */
-  dest->buffer = (JOCTET *)
-    (*cinfo->mem->alloc_small) ((j_common_ptr)cinfo, JPOOL_IMAGE,
+  if (dest->buffer == NULL)
+    dest->buffer = (JOCTET *)
+      (*cinfo->mem->alloc_small) ((j_common_ptr)cinfo, JPOOL_IMAGE,
                                 OUTPUT_BUF_SIZE * sizeof(JOCTET));
 
   dest->pub.next_output_byte = dest->buffer;
@@ -214,6 +215,7 @@ jpeg_stdio_dest(j_compress_ptr cinfo, FILE *outfile)
     cinfo->dest = (struct jpeg_destination_mgr *)
       (*cinfo->mem->alloc_small) ((j_common_ptr)cinfo, JPOOL_PERMANENT,
                                   sizeof(my_destination_mgr));
+    ((my_dest_ptr)cinfo->dest)->buffer = NULL;
   } else if (cinfo->dest->init_destination != init_destination) {
     /* It is unsafe to reuse the existing destination manager unless it was
      * created by this function.  Otherwise, there is no guarantee that the
