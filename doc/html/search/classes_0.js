34938475f5886e701b7c3b90256dcab6d38b9c42
DRC <information@libjpeg-turbo.org>
2020-10-01 14:35:46 -0500

    Dox: Re-generate using Doxygen 1.8.20
    The main purpose of this is to prevent automated security scanning
    software from triggering on the stripped-down version of jquery.js
    generated by Doxygen (even though it is unclear whether that version of
    jquery.js has any real vulnerabilities, given that it is used only for
    static documentation and given that it is stripped down.)

---
+++ doc/html/search/classes_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['tjregion_97',['tjregion',['../structtjregion.html',1,'']]],
+  ['tjscalingfactor_98',['tjscalingfactor',['../structtjscalingfactor.html',1,'']]],
+  ['tjtransform_99',['tjtransform',['../structtjtransform.html',1,'']]]
+];
---
+++ doc/html/search/groups_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['turbojpeg_179',['TurboJPEG',['../group___turbo_j_p_e_g.html',1,'']]]
+];
