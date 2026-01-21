libjpeg-turbo/libjpeg-turbo

commit afcfbb97d29977b80fef3e4b7dc15d27bbf60bea

Fuzz: Consistify usage of alloc/free functions
- Always use tj3Alloc() and tj3Free() for JPEG destination buffers.

- Always use malloc() and free() for other buffers.

Mixing and matching those functions works in the default implementation
of libjpeg-turbo on Un*x platforms, but it isn't guaranteed to work in
all implementations.

Closes #849

fuzz/decompress.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -95,7 +95,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         tj3SetCroppingRegion(handle, TJUNCROPPED);
     }
 
-    if ((dstBuf = tj3Alloc(w * h * tjPixelSize[pf] * sampleSize)) == NULL)
+    if ((dstBuf = malloc(w * h * tjPixelSize[pf] * sampleSize)) == NULL)
       goto bailout;
 
     if (precision == 8) {

fuzz/decompress_yuv.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -83,11 +83,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         tj3SetScalingFactor(handle, TJUNSCALED);
     }
 
-    if ((dstBuf = (unsigned char *)tj3Alloc(w * h * tjPixelSize[pf])) == NULL)
+    if ((dstBuf = (unsigned char *)malloc(w * h * tjPixelSize[pf])) == NULL)
       goto bailout;
-    if ((yuvBuf =
-         (unsigned char *)tj3Alloc(tj3YUVBufSize(w, 1, h,
-                                                 jpegSubsamp))) == NULL)
+    if ((yuvBuf = (unsigned char *)malloc(tj3YUVBufSize(w, 1, h,
+                                                        jpegSubsamp))) == NULL)
       goto bailout;
 
     if (tj3DecompressToYUV8(handle, data, size, yuvBuf, 1) == 0 &&

CMakeLists.txt
@@ -15,7 +15,7 @@ endif()
 
 project(libjpeg-turbo C)
 set(VERSION 3.0.5)
-set(COPYRIGHT_YEAR "1991-2025")
+set(COPYRIGHT_YEAR "1991-2026")
 string(REPLACE "." ";" VERSION_TRIPLET ${VERSION})
 list(GET VERSION_TRIPLET 0 VERSION_MAJOR)
 list(GET VERSION_TRIPLET 1 VERSION_MINOR)

fuzz/compress.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021, 2023-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021, 2023-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -104,7 +104,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         sum += dstBuf[i];
     }
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
     tj3Free(srcBuf);
     srcBuf = NULL;
@@ -116,7 +116,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
+  tj3Free(dstBuf);
   tj3Free(srcBuf);
   if (fd >= 0) {
     close(fd);

fuzz/compress12.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021, 2023-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021, 2023-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -104,7 +104,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         sum += dstBuf[i];
     }
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
     tj3Free(srcBuf);
     srcBuf = NULL;
@@ -116,7 +116,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
+  tj3Free(dstBuf);
   tj3Free(srcBuf);
   if (fd >= 0) {
     close(fd);

fuzz/compress12_lossless.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -101,7 +101,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         sum += dstBuf[i];
     }
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
     tj3Free(srcBuf);
     srcBuf = NULL;
@@ -113,7 +113,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
+  tj3Free(dstBuf);
   tj3Free(srcBuf);
   if (fd >= 0) {
     close(fd);

fuzz/compress16_lossless.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -101,7 +101,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         sum += dstBuf[i];
     }
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
     tj3Free(srcBuf);
     srcBuf = NULL;
@@ -113,7 +113,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
+  tj3Free(dstBuf);
   tj3Free(srcBuf);
   if (fd >= 0) {
     close(fd);

fuzz/compress_lossless.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2021-2025 D. R. Commander.  All Rights Reserved.
+ * Copyright (C)2021-2026 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -100,7 +100,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         sum += dstBuf[i];
     }
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
     tj3Free(srcBuf);
     srcBuf = NULL;
@@ -112,7 +112,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
+  tj3Free(dstBuf);
   tj3Free(srcBuf);
   if (fd >= 0) {
     close(fd);

commit 91ad423edd8c4c70abb30b5fa3de56a409db078c

Always use tj3Alloc()/tj3Free() in decomp. fuzzers
Referring to 562ad7612e4d9ac7735c7d27211f012e8f4ba31e, using tj3Alloc()
to allocate the packed-pixel destination buffers in the decompression
fuzzers was intentional.  Thus, afcfbb97d29977b80fef3e4b7dc15d27bbf60bea
caused a fuzzer regression with MSan.  This re-fixes #849.

fuzz/decompress.cc
@@ -95,7 +95,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         tj3SetCroppingRegion(handle, TJUNCROPPED);
     }
 
-    if ((dstBuf = malloc(w * h * tjPixelSize[pf] * sampleSize)) == NULL)
+    if ((dstBuf = tj3Alloc(w * h * tjPixelSize[pf] * sampleSize)) == NULL)
       goto bailout;
 
     if (precision == 8) {
@@ -126,7 +126,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         goto bailout;
     }
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
 
     /* Prevent the sum above from being optimized out.  This test should never
@@ -136,7 +136,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
+  tj3Free(dstBuf);
   tj3Destroy(handle);
   return 0;
 }

fuzz/decompress_yuv.cc
@@ -83,10 +83,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         tj3SetScalingFactor(handle, TJUNSCALED);
     }
 
-    if ((dstBuf = (unsigned char *)malloc(w * h * tjPixelSize[pf])) == NULL)
+    if ((dstBuf = (unsigned char *)tj3Alloc(w * h * tjPixelSize[pf])) == NULL)
       goto bailout;
-    if ((yuvBuf = (unsigned char *)malloc(tj3YUVBufSize(w, 1, h,
-                                                        jpegSubsamp))) == NULL)
+    if ((yuvBuf =
+         (unsigned char *)tj3Alloc(tj3YUVBufSize(w, 1, h,
+                                                 jpegSubsamp))) == NULL)
       goto bailout;
 
     if (tj3DecompressToYUV8(handle, data, size, yuvBuf, 1) == 0 &&
@@ -98,9 +99,9 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     } else
       goto bailout;
 
-    free(dstBuf);
+    tj3Free(dstBuf);
     dstBuf = NULL;
-    free(yuvBuf);
+    tj3Free(yuvBuf);
     yuvBuf = NULL;
 
     /* Prevent the sum above from being optimized out.  This test should never
@@ -110,8 +111,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   }
 
 bailout:
-  free(dstBuf);
-  free(yuvBuf);
+  tj3Free(dstBuf);
+  tj3Free(yuvBuf);
   tj3Destroy(handle);
   return 0;
 }